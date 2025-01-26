2ab3c4676b24bd4a6c0d8e6d801a8d24e1d701b8
Arsh <120127753+ArshdevSinghji@users.noreply.github.com>
2025-01-26 19:53:51 +0530

    Update Content_Only.js

--- question-search-frontend/src/Components/Questions/Content_Only.js
+++ question-search-frontend/src/Components/Questions/Content_Only.js
@@ -25,7 +25,7 @@ const Anagram = () => {
 
     const fetchingData = async (page = 1, searchQuery = '') => {
         try {
-            const response = await fetch(`http://localhost:5000/api/${selected}?page=${page}&limit=${questionsPerPage}&search=${searchQuery}`);
+            const response = await fetch(`https://question-search-backend.onrender.com/api/${selected}?page=${page}&limit=${questionsPerPage}&search=${searchQuery}`);
             const data = await response.json();
             setQuestions(data.data);
             setTotalPages(data.totalPages);
